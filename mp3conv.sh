acb3e0da4f6ca64d574c07d16fb1e13c66fa18e2
Casimir IV Jagiellon <wilson888888888@gmail.com>
2015-11-10 20:48:37 -0800

    Update mp3conv.sh

--- mp3conv.sh
+++ mp3conv.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
-if [ $# -eq 0 ]; then
-  echo "Please specify quality"
-  exit 1
+if [ $# -le 1 ]; then
+	echo "Please specify quality and format being converted from"
+	exit 1
 fi
 form=$1
+from=$2
 dir="${PWD##*/}"
 dir=""$dir"-mp3-"$form""
 mkdir $dir
 first=${form:0:1}
 echo "Converting all flacs to mp3 $1..."
 if [ $first = "V" ]; then
-  arg="-q:a ${form:1:${#form}-1}"
+	arg="-q:a ${form:1:${#form}-1}"
 else
-  arg="-b:a ${form}k"
+	arg="-b:a ${form}k"
 fi
-for a in *.flac; do
-  ffmpeg -i "$a" $arg "${a[@]/%flac/mp3}"
-done
-mv *.mp3 $dir
+if [ $from = "flac" ]; then
+	for a in *.flac; do
+		ffmpeg -i "$a" $arg "${a[@]/%flac/mp3}"
+	done
+	mv *.mp3 $dir
+else
+	for a in *.mp3; do
+		ffmpeg -i "$a" $arg "${a:0:${#a}-4}-$form.mp3"
+	done
+	mv *$form.mp3 $dir	
+fi
 mv $dir ..
